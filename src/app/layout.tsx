d0658e76a79445c88360a0579006e3f821caba34
Madan Sapkota <namastemadan.sapkota@gmail.com>
2024-12-20 17:15:48 +0545

    Added Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -66,6 +67,7 @@ export default function RootLayout({
           <TooltipProvider delayDuration={0}>
             {children}
             <Analytics />
+            <SpeedInsights />
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
